b48da5fecca0198c7a183ad894e25cedc813e2b1
RPL-NeliaDane <neliadane17@gmail.com>
2022-06-21 16:43:51 +0700

    Github Profile Nelia Dane

---
+++ app/src/main/java/id/ac/polbeng/nelia/githubprofile/services/GithubUserService.kt
@@ -0,0 +1,13 @@
+package id.ac.polbeng.nelia.githubprofile.services
+
+import id.ac.polbeng.nelia.githubprofile.models.GithubUser
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubUserService {
+    @GET("users/{login}")
+    fun loginUser(
+        @Path("login")userLogin:String
+    ):Call<GithubUser>
+}
