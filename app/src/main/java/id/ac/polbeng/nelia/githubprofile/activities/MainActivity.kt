35671fcee2fdbc1c659767289218d93d7c2f1082
RPL-NeliaDane <neliadane17@gmail.com>
2022-06-23 10:40:35 +0700

    Github Profile Nelia Dane

--- app/src/main/java/id/ac/polbeng/nelia/githubprofile/activities/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/nelia/githubprofile/activities/MainActivity.kt
@@ -4,23 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.request.RequestOptions
 import id.ac.polbeng.nelia.githubprofile.GlideApp
 import id.ac.polbeng.nelia.githubprofile.R
-import id.ac.polbeng.nelia.githubprofile.activities.MainActivity.Companion.TAG
 import id.ac.polbeng.nelia.githubprofile.databinding.ActivityMainBinding
 import id.ac.polbeng.nelia.githubprofile.helpers.Config
 import id.ac.polbeng.nelia.githubprofile.models.GithubUser
 import id.ac.polbeng.nelia.githubprofile.services.GithubUserService
 import id.ac.polbeng.nelia.githubprofile.services.ServiceBuilder
+import id.ac.polbeng.nelia.githubprofile.viewmodels.MainViewModel
+import id.ac.polbeng.nelia.githubprofile.viewmodels.MainViewModel.Companion.TAG
 import retrofit2.Call
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    companion object {
-        val TAG: String = MainActivity::class.java.simpleName
-    }
+//    companion object {
+//        val TAG: String = MainActivity::class.java.simpleName
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,48 +31,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+            MainViewModel::class.java)
+        mainViewModel.githubUser.observe(this) { user ->
+            setUserData(user)
+        }
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+
         binding.btnSearchUserLogin.setOnClickListener {
             val userLogin = binding.etSearchUserLogin.text.toString()
+            var query = Config.DEFAULT_USER_LOGIN
             if (userLogin.isNotEmpty()) {
-                searchUser(userLogin)
+                query = userLogin
             }
+            mainViewModel.searchUser(query)
         }
-        searchUser(Config.DEFAULT_USER_LOGIN)
     }
 
-    private fun searchUser(query: String) {
-        showLoading(true)
-        Log.d(TAG, "getDataUserProfileFromAPI: start...")
-        val githubUserService: GithubUserService =
-            ServiceBuilder.buildService(GithubUserService::class.java)
-        val requestCall: Call<GithubUser> = githubUserService.loginUser(query)
-        requestCall.enqueue(object : retrofit2.Callback<GithubUser> {
-            override fun onResponse(
-                call: Call<GithubUser>,
-                response: retrofit2.Response<GithubUser>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val result = response.body()
-                    if (result != null) {
-                        setUserData(result)
-                    }
-                    Log.d(TAG, "getDataUserFromAPI: onResponse finish...")
-                } else {
-                    binding.tvUser.text = "User Not Found"
-                    GlideApp.with(applicationContext)
-                        .load(R.drawable.ic_baseline_broken_image_24)
-                        .into(binding.imgUser)
-                    Log.d(TAG, "getDataUserFromAPI: onResponse failed...")
-                }
-            }
-
-            override fun onFailure(call: Call<GithubUser>, t: Throwable) {
-                showLoading(false)
-                Log.d(TAG, "getDataUserFromAPI: onFailure ${t.message}...")
-            }
-        })
-    }
+//    private fun searchUser(query: String) {
+//        showLoading(true)
+//        Log.d(TAG, "getDataUserProfileFromAPI: start...")
+//        val githubUserService: GithubUserService =
+//            ServiceBuilder.buildService(GithubUserService::class.java)
+//        val requestCall: Call<GithubUser> = githubUserService.loginUser(query)
+//        requestCall.enqueue(object : retrofit2.Callback<GithubUser> {
+//            override fun onResponse(
+//                call: Call<GithubUser>,
+//                response: retrofit2.Response<GithubUser>
+//            ) {
+//                showLoading(false)
+//                if (response.isSuccessful) {
+//                    val result = response.body()
+//                    if (result != null) {
+//                        setUserData(result)
+//                    }
+//                    Log.d(TAG, "getDataUserFromAPI: onResponse finish...")
+//                } else {
+//                    binding.tvUser.text = "User Not Found"
+//                    GlideApp.with(applicationContext)
+//                        .load(R.drawable.ic_baseline_broken_image_24)
+//                        .into(binding.imgUser)
+//                    Log.d(TAG, "getDataUserFromAPI: onResponse failed...")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<GithubUser>, t: Throwable) {
+//                showLoading(false)
+//                Log.d(TAG, "getDataUserFromAPI: onFailure ${t.message}...")
+//            }
+//        })
+//    }
 
     private fun setUserData(githubUser: GithubUser) {
         binding.tvUser.text = githubUser.toString()
--- app/src/main/java/id/ac/polbeng/nelia/githubprofile/helpers/Config.kt
+++ app/src/main/java/id/ac/polbeng/nelia/githubprofile/helpers/Config.kt
@@ -1,8 +1,12 @@
 package id.ac.polbeng.nelia.githubprofile.helpers
+
+import id.ac.polbeng.nelia.githubprofile.BuildConfig
+
 class Config {
    companion object{
        const val SPLASH_SCREEN_DELAY:Long = 3000
        const val BASE_URL="https://api.github.com"
        const val DEFAULT_USER_LOGIN="NeliaDane"
+       const val PERSONAL_ACCESS_TOKEN="token${BuildConfig.ACCESS_TOKEN}"
    }
 }
